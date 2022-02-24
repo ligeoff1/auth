c55d8f67fa97116ccd6b71631229aba002b5281f
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-24 01:55:21 +0000

    updated encryption

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
         #find the user trying to login
         @user = User.where({email: params["email"]})[0]
         if @user
-            if @user.password == params["password"]
+            if BCrypt::Password.new(@user.password) == params["password"] #don't forget to also add this to the sessions controller or you won't be able to re-check the encryption 
                 flash[:notice] = "You logged in!"
                 redirect_to "/companies"           
             else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params["user"])
+    @user.password = BCrypt::Password.create(params["user"]["password"]) #save user password as encrypted version
     @user.save
     redirect_to "/users/#{@user.id}"
   end
