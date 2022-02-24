5335052af72fddedf904f790c54829f84120e2b6
ligeoff1 <geoff.li@kellogg.northwestern.edu>
2022-02-24 02:41:31 +0000

    update with professor code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,20 @@
 class SessionsController < ApplicationController
-
     def new
+        
     end
 
     def create
-        #find the user trying to login
-        @user = User.where({email: params["email"]})[0]
+        # ...
+        # find the user trying to login
+        @user = User.where({ email: params["email"] })[0]
         if @user
-            if BCrypt::Password.new(@user.password) == params["password"] #don't forget to also add this to the sessions controller or you won't be able to re-check the encryption 
+            if BCrypt::Password.new(@user.password) == params["password"]
+                # cookies: {}
+                # session: {}
+                session["user_id"] = @user.id
+
                 flash[:notice] = "You logged in!"
-                redirect_to "/companies"           
+                redirect_to "/companies"
             else
                 flash[:notice] = "Nope."
                 redirect_to "/sessions/new"
@@ -18,7 +23,11 @@ class SessionsController < ApplicationController
             flash[:notice] = "Nope."
             redirect_to "/sessions/new"
         end
-           
     end
 
+    def destroy
+        session["user_id"] = nil
+        flash[:notice] = "Goodbye."
+        redirect_to "/sessions/new"
+    end
 end
